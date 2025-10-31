973c958e751d0e40f747637d7cb3e1364f56421b
lmcasella <lautaromcasella@gmail.com>
2025-10-31 03:11:45 -0300

    feat: Agregar GameManager, AudioManager, Singleton base class y GameEvents

--- Assets/Scripts/GameEvents.cs
+++ Assets/Scripts/GameEvents.cs
@@ -1,18 +1,21 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+using System;
 
-public class GameEvents : MonoBehaviour
+public static class GameEvents
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    // --- Eventos de Player ---
+    // Este evento enviará un int (nuevo valor de vida)
+    public static event Action<int> OnPlayerHealthChanged;
+    // Metodo que otros scripts van a llamar para triggerear el evento
+    // Checkea con '?' si OnPlayerHealthChanged es nulo, o sea si nadie está escuchando
+    public static void ReportPlayerHealthChanged(int newHealth) => OnPlayerHealthChanged?.Invoke(newHealth);
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public static event Action OnPlayerDied;
+    public static void ReportPlayerDied() => OnPlayerDied?.Invoke();
+
+    // --- Eventos de Room y game ---
+    public static event Action OnRoomCleared;
+    public static void ReportRoomCleared() => OnRoomCleared?.Invoke();
+
+    public static event Action<int> OnEnemyKilled;
+    public static void ReportEnemyKilled(int scoreValue) => OnEnemyKilled?.Invoke(scoreValue);
 }
