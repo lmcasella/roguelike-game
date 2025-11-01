ddf12371f1ac9fd4feae19a882dd7a635ce5beab
lmcasella <lautaromcasella@gmail.com>
2025-11-01 04:07:41 -0300

    feat: Implementar movimiento del Player

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,21 +2,54 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(SystemHealth))]
+[RequireComponent(typeof(Rigidbody2D))]
 public class Player : MonoBehaviour, IDamageable
 {
+    [Header("Health")]
     private SystemHealth healthComponent;
 
     // Getter
     // '=>' reemplaza '{ get { return ...; } }'
     public int CurrentHealth => healthComponent.GetCurrentHealth();
 
+    [Header("Movement")]
+    [SerializeField] private float moveSpeed = 8f;
+
+    private Rigidbody2D rb;
+    private Vector2 moveInput;
+
     private void Awake()
     {
         healthComponent = GetComponent<SystemHealth>();
+        rb = GetComponent<Rigidbody2D>();
+
         if (healthComponent == null )
         {
             Debug.LogError("Player is missing a Health component");
         }
+    }
+
+    // Update is called once per frame
+    // Implementar aca inputs
+    void Update()
+    {
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveY = Input.GetAxisRaw("Vertical");
+
+        moveInput = new Vector2(moveX, moveY).normalized;
+
+        if (Input.GetKeyDown(KeyCode.H))
+        {
+            healthComponent.DealDamage(10);
+        }
+    }
+
+    // Implementar aca fisicas
+    private void FixedUpdate()
+    {
+        Vector2 newVelocity = new Vector2(moveInput.x * moveSpeed, moveInput.y * moveSpeed);
+        rb.velocity = newVelocity;
     }
 
     // --- Implementacion del IDamageable ---
@@ -35,11 +68,5 @@ public class Player : MonoBehaviour, IDamageable
         Debug.Log("Player has died");
         GameEvents.ReportPlayerDied();
         Destroy(gameObject);
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
